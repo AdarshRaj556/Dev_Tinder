fa9196f9d5d3d38110c3c87480872ad3b41e80e9
Adarsh Raj <mt1240757@iitd.ac.in>
2025-12-25 03:55:04 +0530

    play with routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const express=require("express");
 const app=express();
 port=7777;
 
-app.get("/test",(req,res)=>{
+app.use("/",(req,res)=>{
+    res.send("hello adarsh"); //ordering matter if this is at the top most route...route below never get chance to execute.
+})
+
+app.use("/test",(req,res)=>{
     res.send("hello world");
 })
-app.get("/hi",(req,res)=>{
+app.use("/hi",(req,res)=>{
     res.send("hi hi hi");
 })
 app.listen(port,()=>{
     console.log("server is listening");
 })
+
