c23d470a0c1b435cabaf016d79475c1fba860e48
Adarsh Raj <mt1240757@iitd.ac.in>
2025-12-25 13:12:34 +0530

    path using regex

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@ const express=require("express");
 const app=express();
 port=7777;
 
+
+//regex
+app.get(/ab?c/,(req,res)=>{ //ac or abc
+    res.send("get using regex");
+})
+
+app.get(/ab*c/,(req,res)=>{
+    res.send("abbbbcddedd");
+})
 app.get("/user",(req,res)=>{
     // res.send({"name":"adarsh raj","age":"19"});
     console.log(req.query);
     res.send("hi"+req.query.name);
 })
-
 // defining dynamic route 
 app.get("/product/:name/:id",(req,res)=>{
     console.log(req.params.name);
