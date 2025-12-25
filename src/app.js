5c46dc7ebe8bf7cc75f3a43cf0c292261f6c7819
Adarsh Raj <mt1240757@iitd.ac.in>
2025-12-26 05:13:00 +0530

    get user by id and emailid and also all users

--- src/app.js
+++ src/app.js
@@ -17,7 +17,45 @@ app.post("/signup", async (req,res)=>{
     }
     // res.send("happy");
 })
-
+app.get("/user/:id", async (req,res)=>{
+    // console.log(req.body);
+    try{
+        const {id}=req.params
+        const user=await User.findById(id);
+        if(!user){
+            res.status(404).send("user not found");
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        res.send("something went wrong "+err);
+    }
+})
+app.get("/user", async (req,res)=>{
+    // console.log(req.body);
+    try{
+        const users=await User.find({firstName : req.body.firstName});
+        if(users.length===0){
+            res.status(404).send("No user found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.send("something went wrong "+err);
+    }
+})
+app.get("/feed",async (req,res)=>{
+    try{
+        const users=await User.find({});
+        if(users.length===0){
+            res.status(404).send("No user found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.send("something went wrong "+err);
+    }
+})
 connectDB()
 .then(()=>{
         app.listen(port,()=>{
