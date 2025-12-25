94a1904df53693abfbb5aa70295ede4f6cfb16c7
Adarsh Raj <mt1240757@iitd.ac.in>
2025-12-26 03:25:40 +0530

    first user data goes to database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express=require("express");
 const connectDB=require("./config/database");
+const User=require("./models/user");
 const app=express();
 port=7777;
 
+app.post("/signup", async (req,res)=>{
+    const user=new User({
+        firstName:"saloni",
+        lastName:"kumari",
+        gender:"female",
+        emailId:"saloni&gmail.com",
+        password:"saloni123",
+        age:17
+    })
+    try{
+        await user.save();
+        res.send("user saved successfully");
+    }catch(err){
+        res.status(400).send("something went wrong "+ err);
+    }
+})
 
 connectDB()
 .then(()=>{
