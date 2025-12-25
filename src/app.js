128a9eaa21c3a9b700eb79a39eb95a2cc2dfa924
Adarsh Raj <mt1240757@iitd.ac.in>
2025-12-26 01:30:28 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ const express=require("express");
 const app=express();
 port=7777;
 
-const {adminaAuth}=require("./middlewares/auth");
-
-app.use("/admin",adminaAuth);
-app.use("/admin/getalldata",(req,res,next)=>{
-    res.send("all data sent");
+app.use("/user/:id",(req,res,next)=>{
+    try{
+        if(req.params.id==="101"){
+            res.send("hello user");
+        }else{
+            throw new Error("not valid userid");
+        }
+    }catch(err){
+        next(err);
+    }
 })
 
-
-
-
+app.use("/",(err,req,res,next)=>{
+    res.status(404).send("userid not found");
+})
 app.listen(port,()=>{
     console.log("server is listening");
 })
